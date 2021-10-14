5fddc16d134e1616d8c0ae6fca43edd97acba2e7
Lalchandra Rajak <ecgc-dev7@cdacmumbai.in>
2021-10-14 16:39:56 +0530

    Upload New File

---
+++ envar_1_.groovy
@@ -0,0 +1,18 @@
+        
+        	Job_Name='test-dms-dms'
+          
+//BE
+  
+          	beGitUrl ='git@10.212.0.139:ecgc/smile/dms/erp_dms_dms_be.git'
+          	beBranch= 'test_dms'
+
+//BE image
+
+        	//beimage='10.212.0.224:8084/erp-bs-sts-be.jar'
+        	beimage='docker-registry.cdacmumbai.in:443/erp-dms-dms-test-be.jar'
+        	
+        	uatbeimage='docker-registry.ecgc.in:443/erp-dms-dms-test-be.jar'
+        	
+fromemail='ecgcdevops@cdac.in'
+toemails='ecgcdevops@cdac.in, mokshika@cdac.in'
+replyto='ecgcdevops@cdac.in'
