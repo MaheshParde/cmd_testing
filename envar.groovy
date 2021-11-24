2e20eb5b9fa0d571d7d63b8ad5c5b4b19301cf9d
Lalchandra Rajak <ecgc-dev7@cdacmumbai.in>
2021-11-24 11:19:09 +0530

    Update envar.groovy

--- envar.groovy
+++ envar.groovy
@@ -1,4 +1,4 @@
-Job_Name='erp-accounts-accounts'
+Job_Name='test-auto'
 
 beGitUrl='git@10.212.0.139:ecgc/smile/accounts/erp_accounts_accounts_be.git'
 beBranch='developer'
@@ -9,12 +9,12 @@ feBranch='developer'
 beimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-be.jar '
 feimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-fe.jar '
 
-uatbeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-be.jar'
-uatfeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-fe.jar'
+//uatbeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-be.jar'
+//uatfeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-fe.jar'
 
 fromemail='ecgc-dev7@cdac.in'
 toemails='ecgcdevops@cdac.in'
-qaemails='ecgc-dev7@cdac.in'
+//qaemails='ecgc-dev7@cdac.in'
 //toemails='ecgcdevops@cdac.in, shwetapal@cdac.in, vrushalic@cdac.in,aakashn@cdac.in,amiteshb@cdac.in,mohasin@cdac.in'
 replyto='ecgc-dev7@cdac.in'
 
