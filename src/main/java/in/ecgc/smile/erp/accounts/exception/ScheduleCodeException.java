3d2786c877eff2aba619bf45ef193a46500972cc
Lalchandra Rajak <61547075+lalchand-rajak@users.noreply.github.com>
2021-12-16 12:22:28 +0530

    removed

--- src/main/java/in/ecgc/smile/erp/accounts/exception/ScheduleCodeException.java
+++
@@ -1,8 +0,0 @@
-package in.ecgc.smile.erp.accounts.exception;
-
-public class ScheduleCodeException extends Exception {
-
-	public ScheduleCodeException(String massage) {
-		super(massage);
-	}
-}
--- src/main/java/in/ecgc/smile/erp/accounts/integrate/service/ExtPaymentAdviceService.java
+++
@@ -1,6 +0,0 @@
-package in.ecgc.smile.erp.accounts.integrate.service;
-
-public interface ExtPaymentAdviceService {
-	
-	String savePaymentAdvice(Integer seqNo,Integer moduleCd, String paymentAdviceNo);
-}
