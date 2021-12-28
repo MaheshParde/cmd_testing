043071073e7cbdead9fa89e0f6902daf4f24abd6
Lalchandra Rajak <ecgc-dev7@cdacmumbai.in>
2021-12-28 16:05:50 +0530

    Update envar.groovy

--- envar.groovy
+++ envar.groovy
@@ -6,8 +6,8 @@ beBranch='developer'
 feGitUrl='git@10.212.0.139:ecgc/smile/accounts/erp_accounts_accounts_fe.git'
 feBranch='developer'
          	
-beimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-be.jar:$BUILD_NUMBER'
-feimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-fe.jar:$BUILD_NUMBER'
+beimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-be.jar:$BUILD_NUMBER:latest'
+feimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-fe.jar:$BUILD_NUMBER:latest'
 
 
 uatbeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-be.jar'
