73ebad3610461ba63d517c3e890525c6c4e66852
Lalchandra Rajak <ecgc-dev7@cdacmumbai.in>
2021-12-29 10:48:12 +0530

    Update envar.groovy

--- envar.groovy
+++ envar.groovy
@@ -15,9 +15,9 @@ beimage='docker-registry.cdacmumbai.in:443/erp-accounts-accounts-be.jar:$version
 // uatbeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-be.jar'
 // uatfeimage='docker-registry.ecgc.in:443/erp-accounts-accounts-fe.jar'
 
-fromemail='ecgc-dev7@cdac.in'
-toemails='ecgc-dev7@cdac.in'
+// fromemail='ecgc-dev7@cdac.in'
+// toemails='ecgc-dev7@cdac.in'
 //qaemails='ecgc-dev7@cdac.in'
 //toemails='ecgcdevops@cdac.in, shwetapal@cdac.in, vrushalic@cdac.in,aakashn@cdac.in,amiteshb@cdac.in,mohasin@cdac.in'
-replyto='ecgc-dev7@cdac.in'
+// replyto='ecgc-dev7@cdac.in'
 
